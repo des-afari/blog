b8569295f3af425ef8eba24e47f02fe60dbe2016
Desmond Afari <67256731+Reeler@users.noreply.github.com>
2022-05-04 23:32:51 +0100

    Index-adds

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import styles from '../styles/Index.module.css'
 import A1 from '../public/img/a1.jpg'
+import A2 from '../public/img/a2.jpg'
+import A3 from '../public/img/a3.jpg'
 import Image from 'next/image'
 
 
@@ -20,7 +22,7 @@ export default function Home() {
           <ul>
             <li>
               <span className={styles.leftMainImg}>
-                {/* 166x194 */}
+                {/* 200x150 */}
                 <Image src={A1} alt='a1' placeholder='blur'></Image>
               </span>
               <span className={styles.leftMainCon}>
@@ -36,13 +38,40 @@ export default function Home() {
               </span>
             </li>
             <li>
-              <span className={styles.leftMainImg}></span>
-              <span className={styles.leftMainCon}></span>
+              <span className={styles.leftMainImg}>
+                {/* 200x150 */}
+                <Image src={A2} alt='a2' placeholder='blur'></Image>
+              </span>
+              <span className={styles.leftMainCon}>
+                <div>
+                  <h3>The Pursuit Of Happiness Is Dog-Shit</h3>
+                  <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed</p>
+                </div>
+                <div>
+                    <small>Motivational</small>
+                    <small>Kim Gerber</small>
+                    <small>30th May</small>
+                </div>
+              </span>
             </li>
             <li>
-              <span className={styles.leftMainImg}></span>
-              <span className={styles.leftMainCon}></span>
+              <span className={styles.leftMainImg}>
+                {/* 200x150 */}
+                <Image src={A3} alt='a3' placeholder='blur'></Image>
+              </span>
+              <span className={styles.leftMainCon}>
+                <div>
+                  <h3>Learn Solidity</h3>
+                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to </p>
+                </div>
+                <div>
+                    <small>Blockchain</small>
+                    <small>Joe Curry</small>
+                    <small>12th August</small>
+                </div>
+              </span>
             </li>
+           
           </ul>
         </div>
       </section>
