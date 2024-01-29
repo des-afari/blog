99cd34bc72fbd72e3dd66e463f4e7c0a038d6958
unknown <afaridesmond@gmail.com>
2024-01-29 09:13:15 +0000

    index skeleton updated

--- client/src/components/skeletons/IndexSkeleton.tsx
+++ client/src/components/skeletons/IndexSkeleton.tsx
@@ -10,47 +10,6 @@ const IndexSkeleton: FC = () => {
         <Skeleton className='h-8 w-24 rounded-md px-3 text-xs'></Skeleton>
         <Skeleton className='h-8 w-24 rounded-md px-3 text-xs'></Skeleton>
       </section>
-      <main style={{minHeight: "calc(100vh - 2.75rem - 4rem"}} className='p-6'>
-        <div className='grid gap-y-12 sm:px-12 md:px-0 md:grid-cols-2 md:gap-6 lg:grid-cols-3 mb-12'>
-          <div className='space-y-3'>
-            <Skeleton className='h-[218px] sm:h-[330px] md:h-[232px] lg:h-[206px] xl:h-[298px]'></Skeleton>
-            <div className='flex items-center flex-wrap gap-2'>
-              <Skeleton className='h-[22px] w-[67px]'></Skeleton>
-              <Skeleton className='h-[22px] w-[67px]'></Skeleton>
-            </div>
-            <Skeleton className='h-[44px]'></Skeleton>
-            <Skeleton className='h-[100px] lg:h-[80px]'></Skeleton>
-          </div>
-          <div className='space-y-3'>
-            <Skeleton className='h-[218px] sm:h-[330px] md:h-[232px] lg:h-[206px] xl:h-[298px]'></Skeleton>
-            <div className='flex items-center flex-wrap gap-2'>
-              <Skeleton className='h-[22px] w-[67px]'></Skeleton>
-              <Skeleton className='h-[22px] w-[67px]'></Skeleton>
-            </div>
-            <Skeleton className='h-[44px]'></Skeleton>
-            <Skeleton className='h-[100px] lg:h-[80px]'></Skeleton>
-          </div>
-          <div className='space-y-3'>
-            <Skeleton className='h-[218px] sm:h-[330px] md:h-[232px] lg:h-[206px] xl:h-[298px]'></Skeleton>
-            <div className='flex items-center flex-wrap gap-2'>
-              <Skeleton className='h-[22px] w-[67px]'></Skeleton>
-              <Skeleton className='h-[22px] w-[67px]'></Skeleton>
-            </div>
-            <Skeleton className='h-[44px]'></Skeleton>
-            <Skeleton className='h-[100px] lg:h-[80px]'></Skeleton>
-          </div>
-          <div className='space-y-3'>
-            <Skeleton className='h-[218px] sm:h-[330px] md:h-[232px] lg:h-[206px] xl:h-[298px]'></Skeleton>
-            <div className='flex items-center flex-wrap gap-2'>
-              <Skeleton className='h-[22px] w-[67px]'></Skeleton>
-              <Skeleton className='h-[22px] w-[67px]'></Skeleton>
-            </div>
-            <Skeleton className='h-[44px]'></Skeleton>
-            <Skeleton className='h-[100px] lg:h-[80px]'></Skeleton>
-          </div>
-          
-        </div>
-      </main>
     </div>
   )
 }
